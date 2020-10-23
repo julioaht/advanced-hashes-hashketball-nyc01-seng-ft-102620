a23d387afd1437a76b58c8df358ca038953456fd
julio hernandez <julioandres72@gmail.com>
2020-10-23 03:34:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,7 +177,7 @@ def big_shoe_rebounds
   biggest = 0
   rebounds = 0
   game_hash.each do |home_away, keys|
-    keys[:player].each do |player|
+    keys[:players].each do |player|
       size = player[:shoe]
       if size > biggest
         biggest = size
